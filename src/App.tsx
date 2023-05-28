8b5ef44d14ebfb5316da6aa73a2508e99781c75e
Rui Dias <rui.ldias@ceiia.com>
2023-05-28 11:13:02 +0100

    first blood

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return <>
+      <h1 className="">Coffe Delivery</h1>
+    </>;
+}
+
+export default App;
